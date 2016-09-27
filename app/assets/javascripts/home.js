2be2266cdf90d83b2b882c22d81a0891bd31b258
urvi57 <urvashi.gkmit.co>
2016-09-27 16:38:28 +0530

    Dummy change

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+var SS = SS || {};
+
+SS.Home = function(base_url) {
+ this.initialize();
+}
+
+SS.Home.prototype = {
+ initialize: function () {
+   alert("Hello");
+ }
+}
